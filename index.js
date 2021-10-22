2459b63bd103019b3f1e09c9d15e0e755ef82b1c
Abhishek Sharma <abhishekalien09@gmail.com>
2021-10-22 07:09:08 +0530

    deploy

---
+++ index.js
@@ -0,0 +1,192 @@
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken")
+const express = require("express");
+const admin = require("firebase-admin");
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const keys = require("./keys.json");
+const port = process.env.PORT || 3000 ;
+admin.initializeApp({
+    credential: admin.credential.cert(keys),
+    databaseURL: "https://teamcollabarator-default-rtdb.firebaseio.com",
+});
+
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(cookieParser());
+app.use(express.json());
+
+
+let rooms = {}
+let chats = {};
+let files = {};
+
+
+
+
+app.get('/', (req, res) => {
+    res.redirect('/home')
+})
+app.get('/home', (req, res) => {
+
+    jwt.verify(req.cookies.jwt, 'secretkey', (err, authData) => {
+        if (err) {
+            res.render('index', { rooms: rooms })
+        } else {
+            curr_auth = authData;
+
+            res.redirect('/dashboard')
+        }
+    });
+
+})
+
+app.get('/signUp', (req, res) => {
+    res.render("signup");
+})
+app.get('/logIn', (req, res) => {
+    res.render("login");
+})
+
+app.get("/profile", function (req, res) {
+    jwt.verify(req.cookies.jwt, 'secretkey', (err, authData) => {
+        if (err) {
+            res.redirect("/logIn")
+        } else {
+            curr_auth = authData;
+            res.render("profile", { curr_auth: JSON.stringify(authData) });
+        }
+    });
+});
+
+
+app.post("/sessionLogin", (req, res) => {
+
+    const user = {
+        name: req.body.username,
+        email: req.body.email
+    }
+    jwt.sign({ user }, 'secretkey', (err, token) => {
+        console.log(token)
+        res.cookie("jwt", token, {
+            expires: new Date(Date.now() + 1000 * 60 * 60 * 24),
+            path: "/"
+        })
+        res.json({ token })
+
+    })
+
+
+});
+
+app.get("/sessionLogout", (req, res) => {
+    res.clearCookie("jwt", {
+        path: "/",
+        domain: "localhost"
+    });
+    res.sendStatus(200);
+    console.log("User logged out")
+});
+
+
+let curr_auth;
+app.get('/dashboard', (req, res) => {
+
+    jwt.verify(req.cookies.jwt, 'secretkey', (err, authData) => {
+        if (err) {
+            res.redirect("/logIn")
+        } else {
+            curr_auth = authData;
+
+            res.render("dashboard", { rooms, curr_auth: JSON.stringify(authData) });
+        }
+    });
+
+
+})
+
+app.post('/room', (req, res) => {
+    if (rooms[req.body.room] != null) {
+        console.log("Room already exists already");
+        return res.redirect('/');
+    }
+    rooms[req.body.room] = { users: {} };
+    chats[req.body.room] = [];
+    res.redirect(req.body.room);
+    io.emit('room-created', req.body.room);
+
+})
+
+app.get('/:room', (req, res) => {
+    if (rooms[req.params.room] == null) {
+        return res.redirect('/home')
+    }
+
+
+    jwt.verify(req.cookies.jwt, 'secretkey', (err, authData) => {
+        if (err) {
+            res.render("rooms", { roomName: req.params.room, chatObj: JSON.stringify(chats), curr_auth: null, roomslist: rooms, files: JSON.stringify(files) });
+        } else {
+            curr_auth = authData;
+            res.render('rooms', { roomName: req.params.room, chatObj: JSON.stringify(chats), curr_auth: JSON.stringify(authData), roomslist: rooms, files: JSON.stringify(files) });
+        }
+    });
+
+})
+
+
+server.listen(port );
+
+io.on('connection', socket => {
+
+    socket.on('new-user', (room, name) => {
+        socket.join(room)
+        rooms[room].users[socket.id] = name
+        socket.to(room).emit('user-connected', name, rooms[room].users);
+        console.log(rooms);
+
+        io.to(room).emit('roomUsers', {
+            room: room,
+            users: rooms[room].users
+        })
+    })
+
+    socket.on('send-chat-message', (room, message) => {
+        socket.to(room).emit('chat-message', { message: message, name: rooms[room].users[socket.id] })
+        chats[room].push({ name: rooms[room].users[socket.id], message: message });
+
+    })
+
+    socket.on('disconnect', () => {
+        getUserRooms(socket).forEach(room => {
+            socket.to(room).emit('user-disconnected', rooms[room].users[socket.id])
+            delete rooms[room].users[socket.id];
+            io.to(room).emit('roomUsers', {
+                room: room,
+                users: rooms[room].users
+            })
+            console.log(files)
+        })
+    })
+
+    socket.on('text-change', (delta, room, message) => {
+        if (files[room] != null) files[room].push(delta)
+        else { files[room] = []; files[room].push(delta) }
+        console.log(delta);
+        socket.to(room).emit('receive-changes', delta);
+    })
+
+})
+
+function getUserRooms(socket) {
+    return Object.entries(rooms).reduce((names, [name, room]) => {
+        if (room.users[socket.id] != null) names.push(name)
+        return names
+    }, [])
+}
---
+++ public/script.js
@@ -0,0 +1,197 @@
+const socket = io('http://localhost:3000');
+
+const messageContainer = document.getElementById('message-container');
+const messageForm = document.getElementById('send-container');
+const messageInput = document.getElementById('message-input');
+const roomContainer = document.getElementById('roomContainer');
+
+
+
+const toolbar_options = [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ font: [] }],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["bold", "italic", "underline"],
+    [{ color: [] }, { background: [] }],
+    [{ script: "sub" }, { script: "supe" }],
+    [{ align: [] }],
+    ["image", "blockquote", "code-block"],
+];
+let userList = [];
+let quill = "";
+let nameuser = "";
+
+let temp = [];
+
+
+if (messageForm != null) {
+    // var chats = JSON.parse(!(JSON.stringify(chatObj)));
+    chats = chatObj.replaceAll('&#34;', '"')
+    console.log(JSON.parse(chats));
+
+    if (user !== null && typeof user == 'object') {
+
+        nameuser = user.user.name;
+        console.log(nameuser)
+    }
+    else {
+        while (nameuser === "") {
+            nameuser = prompt("Enter your name");
+        }
+    }
+    document.querySelector(".leave").addEventListener('click', (e) => {
+        e.preventDefault();
+        window.location.assign('/')
+    })
+    appendMessage('You Joined...', "right");
+    socket.emit('new-user', roomName, nameuser)
+    document.title = roomName
+
+
+    messageForm.addEventListener('submit', e => {
+        e.preventDefault();
+        const message = messageInput.value;
+        appendformatMessage({ name: "You", message: message }, "right");
+        socket.emit('send-chat-message', roomName, message);
+        messageInput.value = "";
+    })
+
+    getChats(roomName);
+    quill = new Quill('#editor', {
+        theme: 'snow',
+        modules: { toolbar: toolbar_options }
+    });
+
+    addquillEvent().then(() => {
+        quill.on('text-change', function (delta, oldDelta, source) {
+            const message = document.querySelector('.ql-editor').innerText;
+            if (source === 'user') {
+                socket.emit("text-change", delta, roomName, message);
+                // temp.push(delta);
+                // localStorage.setItem('filecontent', temp)
+            }
+
+        });
+    });
+
+}
+
+async function addquillEvent() {
+    fileRoom = files.replaceAll("&#34;", '"');
+    fileRoom = fileRoom.replaceAll("\n", "\\n");
+
+    console.log(fileRoom)
+    fileRoom = JSON.parse(fileRoom);
+
+
+    change = [];
+    if (fileRoom[roomName] != null) {
+        for (let i = 0; i < fileRoom[roomName].length; i++) {
+            quill.updateContents(fileRoom[roomName][i]);
+            // change.push(fileRoom[roomName][i].ops);
+        }
+        // console.log(change);
+
+    }
+    //     document.querySelector('.ql-editor').innerText = fileRoom[roomName];
+    return 0;
+}
+
+
+
+socket.on('receive-changes', (delta) => {
+    quill.updateContents(delta);
+    socket.off('text-change');
+
+});
+
+socket.on('chat-message', data => {
+    if (data.name == nameuser)
+        appendformatMessage({ name: data.name, message: data.message }, "right");
+    else
+        appendformatMessage({ name: data.name, message: data.message }, "left");
+
+});
+
+socket.on('user-connected', (nameuser, allusers) => {
+    appendMessage(`${nameuser} connected...`, "left");
+
+})
+
+socket.on("roomUsers", ({ room, users }) => {
+    displayUser(users);
+})
+
+socket.on('user-disconnected', nameuser => {
+    localStorage.setItem('chats', JSON.stringify(chats));
+    localStorage.setItem('files', JSON.stringify(files));
+    appendMessage(`${nameuser} disconnected...`, "right");
+})
+
+
+socket.on('room-created', room => {
+
+    const roomElement = document.createElement('div');
+    roomElement.className = "roomList";
+    roomElement.innerHTML = `
+    <span class="badge bg-warning text-dark">
+                    ${room}
+                </span>
+    <a class="btn btn-sm btn-primary" href="/${room}" role="button">Join</a>
+    
+    `;
+    roomContainer.append(roomElement);
+
+
+
+})
+
+
+function appendMessage(message, position) {
+    const messageElement = document.createElement('div');
+    messageElement.className = position;
+    messageElement.style.display = "block";
+    messageElement.innerText = message;
+    console.log(messageElement);
+    messageContainer.append(messageElement);
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+}
+
+function appendformatMessage({ name, message }, position) {
+    const messageElement = document.createElement('div');
+    messageElement.className = position;
+    messageElement.innerHTML = `<div class="name"><span class="badge rounded-pill bg-warning text-dark">${name}</span></div>
+    <div class="actualMessage">
+        ${message}
+    </div>`;
+    messageContainer.append(messageElement);
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+}
+
+function getChats(roomName) {
+    chats = JSON.parse(chatObj.replaceAll('&#34;', '"'));
+    for (let i = 0; i < chats[roomName].length; i++) {
+        let name = chats[roomName][i].name;
+        let message = chats[roomName][i].message;
+        if (name == nameuser) {
+            appendformatMessage({ name, message }, "right");
+        }
+        else {
+            appendformatMessage({ name, message }, "left");
+        }
+
+    }
+}
+
+function displayUser(users) {
+    document.querySelector(".dropdown-menu").innerHTML = "";
+    Object.entries(users).forEach((user) => {
+        const ele = document.createElement('li');
+        ele.innerHTML = `<a class="dropdown-item">${user[1]}</a>`;
+        ele.innerText = user[1];
+        document.querySelector(".dropdown-menu").appendChild(ele);
+    })
+}
+
+
+
