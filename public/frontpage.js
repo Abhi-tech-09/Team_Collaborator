0f45df916551318c0ccabbea84af0ba46d8942d4
Abhishek Sharma <abhishekalien09@gmail.com>
2021-10-29 10:54:30 +0530

    Google Auth just added

--- public/frontpage.js
+++ public/frontpage.js
@@ -41,3 +41,35 @@ socket.on('room-deleted', room => {
 function deleteRoom(e) {
     socket.emit("delete-room", e.target.value);
 }
+
+
+
+document.querySelector('.google').addEventListener('click', e => {
+    e.preventDefault();
+    firebase.initializeApp(firebaseConfig);
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+            // /** @type {firebase.auth.OAuthCredential} */
+            var credential = result.credential;
+
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            console.log(user)
+            alert('working');
+            // ...
+        }).catch((error) => {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+        });
+
+})
