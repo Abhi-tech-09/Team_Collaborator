914f5724cd62e0f241c37d489a69eddd87bc5740
Abhishek Sharma <abhishekalien09@gmail.com>
2021-10-29 11:13:42 +0530

    Succesfully added google_auth and created token on backend

--- public/frontpage.js
+++ public/frontpage.js
@@ -43,33 +43,3 @@ function deleteRoom(e) {
 }
 
 
-
-document.querySelector('.google').addEventListener('click', e => {
-    e.preventDefault();
-    firebase.initializeApp(firebaseConfig);
-    var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth()
-        .signInWithPopup(provider)
-        .then((result) => {
-            // /** @type {firebase.auth.OAuthCredential} */
-            var credential = result.credential;
-
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            var token = credential.accessToken;
-            // The signed-in user info.
-            var user = result.user;
-            console.log(user)
-            alert('working');
-            // ...
-        }).catch((error) => {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            // ...
-        });
-
-})
